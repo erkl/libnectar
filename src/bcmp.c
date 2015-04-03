01adea6a205e37ad6da5ac55a8ec1b5046a35b91
Erik Lundin <erik@kvick.co>
2015-04-03 10:57:32 +0100

    Replace branching at the end of nectar_bcmp with a simple cast

--- src/bcmp.c
+++ src/bcmp.c
@@ -41,5 +41,5 @@ int nectar_bcmp(const uint8_t * buf0, const uint8_t * buf1, size_t len) {
     for (i = 0; i < len; i++)
         r |= (buf0[i] ^ buf1[i]);
 
-    return (r == 0 ? 0 : -1);
+    return (int) r;
 }
